d79f1abfd1c9798ebd75074c39caee6099727e4c
Carlos Morales <carlos@cmorales.es>
2015-10-14 12:12:16 +0200

    Create 2015_10_14_191823_create_blog_post_media_table.php

---
+++ migrations/2015_10_14_191823_create_blog_post_media_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBlogPostMediaTables extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_post_media', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('media')->nullable();
+            $table->integer('sort');
+            $table->boolean('enabled');
+
+            $table->integer('blog_post_id')->unsigned();
+        });
+
+        Schema::table('blog_post_media', function (Blueprint $table) {
+            $table->foreign('blog_post_id')
+                ->references('id')->on('blog_posts')
+                ->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_post_media');
+    }
+
+}
