fc4aa962f18451d37ca41a22231abc2f0dd3d459
Carlos Morales <carlos@cmorales.es>
2015-10-14 12:02:04 +0200

    Add main image to post

--- migrations/2015_07_29_191732_create_blog_posts_table.php
+++ migrations/2015_07_29_191732_create_blog_posts_table.php
@@ -14,6 +14,7 @@ class CreateBlogPostsTable extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('image')->nullable();
 
             $table->unsignedInteger('author_id');
             $table->unsignedInteger('category_id')->nullable();
